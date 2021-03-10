911ab4d619984bac53218abf2a548e807cf0041f
Lucas-rbnt <luca.robinet@gmail.com>
2021-03-10 22:02:58 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+import os
+from typing import List
+
+
+PATH_ROOT = os.path.dirname(__file__)
+
+
+def _load_requirements(path_dir: str, file_name: str) -> List[str]:
+    with open(os.path.join(path_dir, file_name), "r") as file:
+        lines = [ln.strip() for ln in file.readlines()]
+    reqs = list()
+    for line in lines:
+        if line.startswith("#"):
+            continue
+        else:
+            reqs.append(line)
+    return reqs
+
+
+setup(
+    name="mlutils",
+    version="1.0.0",
+    author="Lucas Robinet",
+    author_email="lucas.robinet@yahoo.com",
+    description="Machine Learning Toolbox",
+    long_description=open("README.md", "r").read(),
+    long_description_content_type="text/markdown",
+    url="",
+    license="Apache-2.0",
+    packages=find_packages(exclude=["examples", "examples.*", "tests", "tests.*"]),
+    install_requires=_load_requirements(
+        path_dir=os.path.join(PATH_ROOT), file_name="requirements.txt"
+    ),
+    extras_requires={
+        "dev": _load_requirements(
+            path_dir=os.path.join(PATH_ROOT), file_name="requirements-dev.txt"
+        ),
+    },
+)
---
+++ vision/preprocessing/preprocessors.py
@@ -0,0 +1,112 @@
+# import the necessary packages
+import cv2
+from sklearn.feature_extraction.image import extract_patches_2d
+from tensorflow.keras.preprocessing.image import img_to_array
+import numpy as np
+import imutils
+
+
+class SimplePreprocessor:
+    def __init__(self, width, height, inter=cv2.INTER_AREA):
+        self.width = width
+        self.height = height
+        self.inter = inter
+
+    def preprocess(self, image):
+
+        return cv2.resize(image, (self.width, self.height), interpolation=self.inter)
+
+
+class SimpleScaler:
+    def __init__(self, max=255.0):
+        self.max = max
+
+    def preprocess(self, image):
+        return image / self.max
+
+
+class PatchPreprocessor:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def preprocess(self, image):
+        return extract_patches_2d(image, (self.height, self.width), max_patches=1)[0]
+
+
+class MeanPreprocessor:
+    def __init__(self, rMean, gMean, bMean):
+        self.rMean = rMean
+        self.gMean = gMean
+        self.bMean = bMean
+
+    def preprocess(self, image):
+        (B, G, R) = cv2.split(image.astype("float32"))
+
+        R -= self.rMean
+        G -= self.gMean
+        B -= self.bMean
+
+        return cv2.merge([B, G, R])
+
+
+class ImageToArrayPreprocessor:
+    def __init__(self, dataFormat=None):
+        self.dataFormat = dataFormat
+
+    def preprocess(self, image):
+        return img_to_array(image, data_format=self.dataFormat)
+
+
+class CropPreprocessor:
+    def __init__(self, width, height, horiz=True, inter=cv2.INTER_AREA):
+        self.width = width
+        self.height = height
+        self.horiz = horiz
+        self.inter = inter
+
+    def preprocess(self, image):
+        crops = []
+        (h, w) = image.shape[:2]
+        coords = [
+            [0, 0, self.width, self.height],
+            [w - self.width, 0, w, self.height],
+            [w - self.width, h - self.height, w, h],
+            [0, h - self.height, self.width, h],
+        ]
+
+        dW = int(0.5 * (w - self.width))
+        dH = int(0.5 * (h - self.height))
+        coords.append([dW, dH, w - dW, h - dH])
+        for (startX, startY, endX, endY) in coords:
+            crop = image[startY:endY, startX:endX]
+            crop = cv2.resize(crop, (self.width, self.height), interpolation=self.inter)
+            crops.append(crop)
+
+        if self.horiz:
+            # compute the horizontal mirror flips for each crop
+            mirrors = [cv2.flip(c, 1) for c in crops]
+            crops.extend(mirrors)
+        # return the set of crops
+        return np.array(crops)
+
+
+class AspectAwarePreprocessor:
+    def __init__(self, width, height, inter=cv2.INTER_AREA):
+        self.width = width
+        self.height = height
+        self.inter = inter
+
+    def preprocess(self, image):
+        (h, w) = image.shape[:2]
+        dW = 0
+        dH = 0
+        if w < h:
+            image = imutils.resize(image, width=self.width, inter=self.inter)
+            dH = int((image.shape[0] - self.height) / 2.0)
+        else:
+            image = imutils.resize(image, height=self.height, inter=self.inter)
+            dW = int((image.shape[1] - self.width) / 2.0)
+        (h, w) = image.shape[:2]
+        image = image[dH : h - dH, dW : w - dW]
+        return cv2.resize(image, (self.width, self.height), interpolation=self.inter)
